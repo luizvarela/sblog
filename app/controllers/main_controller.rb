b502045740252ebae17304a0bc6eab2a1d5352a8
luiz <luiz.lvarela@gmail.com>
2012-08-27 09:08:39 -0300

    css update

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,19 @@
 class MainController < ApplicationController
   def index
+    @posts = Post.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @posts }
+    end
   end
 
   def list
+    @posts = Post.find(:all,
+        :conditions => "status = 'published'",
+        :order => 'created_at DESC')
+
+    
   end
 
   def category
@@ -12,5 +23,6 @@ class MainController < ApplicationController
   end
 
   def view_post
+    @post = Post.find(params[:id])
   end
 end
